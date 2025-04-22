c50ad07395f3509d03ff7e2f1d6a016e3fc172f6
diazbm <105998374+diazbm@users.noreply.github.com>
2025-04-21 21:00:02 -0300

    Delete src directory

--- src/frontend/dam/src/app/pipes/build-url.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BuildUrlPipe } from './build-url.pipe';
-
-describe('BuildUrlPipe', () => {
-  it('create an instance', () => {
-    const pipe = new BuildUrlPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/frontend/dam/src/app/utils/measurement.ts
+++
@@ -1,5 +0,0 @@
-export class Measurement {
-  static getRandom() {
-    return Math.round(Math.random() * (100 - 10) + 10).toString()
-  }
-}
